e51da8c355b2f0d3c2f8dd1170f8cfc378b059fe
TrVanChinh <chinhtran0964758845@gmail.com>
2023-12-06 23:14:42 +0700

    first commit

--- App.js
+++ App.js
@@ -1,12 +1,37 @@
+// use navigation:
+// npm install @react-navigation/native
+// npx expo install react-native-screens react-native-safe-area-context
+// npm install @react-navigation/native-stack
+// npm install @react-navigation/bottom-tabs
+
+
+// use react native element:
+// npm install react-native-elements
+// npm install react-native-vector-icons
+
+
+//npm i axios
+
+//npm install --save @react-native-firebase/app
+
+//create slide:
+//npm install react-native-swiper
+
+//login google
+//npx expo install expo-dev-client
+//eas build --profile development --platform android
+//npx expo install @react-native-google-signin/google-signin
+//npm i @react-native-firebase/auth
+//npx expo start --dev-client
+
+
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
+import ProfileScreen from './screens/ProfileScreen';
+import StackNavigator from './navigation/StackNavigator';
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <StackNavigator/>
   );
 }
 
